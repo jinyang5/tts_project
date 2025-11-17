f03af51eef53ef0973ccd9c2e192ef5a9565d76a
JinyangHe <1434145913@qq.com>
2025-11-17 15:13:31 +0800

    update download script

--- download_qwen3.py
+++ download_qwen3.py
@@ -1,13 +1,13 @@
-# from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-7B-Instruct")
-# tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
-from transformers import Qwen2AudioForConditionalGeneration, AutoProcessor
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-7B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
+# from transformers import Qwen2AudioForConditionalGeneration, AutoProcessor
 
-model_id = "Qwen/Qwen2-Audio-7B"
+# model_id = "Qwen/Qwen2-Audio-7B"
 
-# 第一次运行时会从 HuggingFace 下载到本地缓存
-model = Qwen2AudioForConditionalGeneration.from_pretrained(model_id)
-processor = AutoProcessor.from_pretrained(model_id)
+# # 第一次运行时会从 HuggingFace 下载到本地缓存
+# model = Qwen2AudioForConditionalGeneration.from_pretrained(model_id)
+# processor = AutoProcessor.from_pretrained(model_id)
 
-print("Qwen2-Audio 下载并加载完成 ✅")
+# print("Qwen2-Audio 下载并加载完成 ✅")
